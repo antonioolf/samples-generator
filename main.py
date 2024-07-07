c199316638077857b24041a8c086a091c0aa236e
Antônio Oliveira <antonio.filho128@gmail.com>
2024-07-07 15:36:23 -0300

    Finished script

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import subprocess
 from glob import glob
 
@@ -47,7 +48,7 @@ def get_tones_between(start_note=None, end_note=None, ascending=True):
 
 
 def generate_a_semitone_higher(origin_tone_path, generated_tone_path):
-    command = (f'ffmpeg -hide_banner -i {origin_tone_path} '
+    command = (f'ffmpeg -hide_banner -loglevel error -i {origin_tone_path} '
                f'-af "rubberband=pitch=1.0595" '
                f'{generated_tone_path}')
     os.system(command)
@@ -60,27 +61,13 @@ def generate_a_semitone_lower(origin_tone_path, generated_tone_path):
     os.system(command)
 
 
-def generate_tones_asc(tones):
-    for _, i in enumerate(tones):
-        origin_tone = tones[i]
-        generated_tone = tones[i + 1]
-        generate_a_semitone_higher(origin_tone, generated_tone)
+def generate_tones_asc(tones, origin):
+    print(f'generate_tones_asc - Gerando os tons {tones} a partir de {origin}')
 
+    origin_tone_path = f'{INPUT_FOLDER}/{origin}.wav'
+    generated_tone_path = f'{OUTPUT_FOLDER}/{tones[0]}.wav'
+    generate_a_semitone_higher(origin_tone_path, generated_tone_path)
 
-def generate_tones_desc(tones, origin):
-    print(f'generate_tones_desc - Gerando os tons {tones} a partir de {origin}')
-
-    tones.reverse()
-
-    # A primeira geração considera o origin como o arquivo que veio da pasta input_samples
-    generated_tone = tones[0]
-    generate_a_semitone_lower(
-        f'{INPUT_FOLDER}/{origin}.wav',
-        f'{OUTPUT_FOLDER}/{generated_tone}.wav'
-    )
-
-    # Da segunda geração em diante considera o origin como os da pasta
-    # output_result que vão ir sendo gerados no loop
     for i, _ in enumerate(tones):
         if (i + 1) == len(tones):
             break
@@ -93,7 +80,32 @@ def generate_tones_desc(tones, origin):
         )
         print('.', end='')
 
-    print('\ngenerate_tones_desc - DONE')
+    print('\ngenerate_tones_asc - DONE\n')
+
+
+def generate_tones_desc(tones, origin):
+    print(f'generate_tones_desc - Gerando os tons {tones} a partir de {origin}')
+    tones.reverse()
+
+    # primeira geração usa a pasta input_samples, da segunda em diante usa a output_result
+    # usando como origin o último áudio gerado no loop
+    origin_tone_path = f'{INPUT_FOLDER}/{origin}.wav'
+    generated_tone_path = f'{OUTPUT_FOLDER}/{tones[0]}.wav'
+    generate_a_semitone_lower(origin_tone_path, generated_tone_path)
+
+    for i, _ in enumerate(tones):
+        if (i + 1) == len(tones):
+            break
+
+        origin_tone = tones[i]
+        generated_tone = tones[i + 1]
+        generate_a_semitone_lower(
+            f'{OUTPUT_FOLDER}/{origin_tone}.wav',
+            f'{OUTPUT_FOLDER}/{generated_tone}.wav'
+        )
+        print('.', end='')
+
+    print('\ngenerate_tones_desc - DONE\n')
 
 
 def sort_musical_notes(notes):
@@ -113,29 +125,52 @@ def sort_musical_notes(notes):
     return sorted_notes
 
 
-def get_first_tones_between(input_notes):
+def get_first_tones_until(first_input_note):
     all_tones = get_all_tones()
-    first_input_note_idx = all_tones.index(input_notes[0])
+    first_input_note_idx = all_tones.index(first_input_note)
     return all_tones[0:first_input_note_idx]
 
 
+def get_last_tones_from(last_input_note):
+    all_tones = get_all_tones()
+    last_input_note_idx = all_tones.index(last_input_note)
+    return all_tones[last_input_note_idx:]
+
+
+def copy_input_files_to_output_folder():
+    print('Copiando input_samples para a pasta output_result')
+    for filename in glob(os.path.join(INPUT_FOLDER, '*.wav')):
+        shutil.copy(filename, OUTPUT_FOLDER)
+
+
 def main():
+    os.makedirs(OUTPUT_FOLDER, exist_ok=True)
     input_notes = sort_musical_notes(get_input_notes())
 
-    # Caso a primeira input note não for c_1 já devem ser gerados tons a partir de c_1 até a
-    # primeira nota mais grave (que é mais aguda que c_1)
+    # Caso a primeira input note não for c_1 deverão ser gerados tons a partir de c_1 até a
+    # primeira input_note mais grave
     if input_notes[0] != "c_1":
         origin = input_notes[0]
-        first_tones = get_first_tones_between(input_notes)
+        first_tones = get_first_tones_until(origin)
         generate_tones_desc(first_tones, origin)
 
     # Continua gerando as notas, agora considerando os gaps existentes no array input_notes
-    # for i, input_note in enumerate(input_notes):
-    #     tones_between = get_tones_between(input_note, input_notes[i + 1])
-    #     generate_tones_asc(tones_between)
+    for i, input_note in enumerate(input_notes):
+        if (i + 1) == len(input_notes):
+            break
+        tones_between = get_tones_between(input_note, input_notes[i + 1])
+        generate_tones_asc(tones_between, input_note)
 
-    # TODO: Assim como o início será preciso gerar os tons que restarem entre a última
-    #  input_note e a última nota definitiva (b_7)
+    # Semelhante ao início, caso a última input_note não for b_7 deverão ser gerados
+    # tons a partir da última input_note até b_7
+    if input_notes[-1] != "b_7":
+        origin = input_notes[-1]
+        last_tones = get_last_tones_from(origin)
+        generate_tones_desc(last_tones, origin)
+
+    copy_input_files_to_output_folder()
+
+    print('Done!')
 
 
 if __name__ == "__main__":
